656480cdd932c078c9f921be42c187c79f534473
til-amsoft <ams281@cam.ac.uk>
2020-07-31 13:57:48 +0100

    Added Basic Calculator Project
    Adding previously created project

---
+++ Basic Calculator/functions.cpp
@@ -0,0 +1,28 @@
+/*
+ * functions.cpp
+ */
+
+double addition(double a, double b){
+	double result = a + b;
+	return result;
+}
+
+double subtraction(double a, double b){
+	double result = a - b;
+	return result;
+}
+
+
+double multiplication(double a, double b){
+	double result = a * b;
+	return result;
+}
+
+
+double division(double a, double b){
+	double result = a / b;
+	return result;
+}
+
+
+
---
+++ Basic Calculator/functions.hpp
@@ -0,0 +1,17 @@
+/*
+ * functions.hpp
+ */
+
+//#ifndef FUNCTIONS_HPP_
+//#define FUNCTIONS_HPP_
+
+double addition(double a, double b);
+
+double subtraction(double a, double b);
+
+double multiplication(double a, double b);
+
+double division(double a, double b);
+
+
+//#endif /* FUNCTIONS_HPP_ */
