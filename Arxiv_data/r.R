56bb8f5e61189e6586fd74403fae4d22f78116a7
til-amsoft <ams281@cam.ac.uk>
2021-05-09 18:32:00 +0100

    Update r.R

--- Arxiv_data/r.R
+++ Arxiv_data/r.R
@@ -1,4 +1,47 @@
-setwd("~/GitHub/Practice/Arxiv_data")
+library(readr)
+library(dplyr)
+library("ggplot2")
+library("RColorBrewer")
+
+Initial_DSC <- read_csv("Part III Project/Initial_DSC.csv")
+DSC<-Initial_DSC %>% filter(Initial_DSC$Segment == 2 |Initial_DSC$Segment == 4 |Initial_DSC$Segment == 6)
+Heat1<-Initial_DSC %>% filter(Initial_DSC$Segment == 2)
+Cool <- Initial_DSC %>% filter(Initial_DSC$Segment == 4)
+Heat2 <- Initial_DSC %>% filter(Initial_DSC$Segment == 6)
+
+ggplot(
+  data = DSC) + 
+  geom_line(aes(x=Temp, y=DSC, color=as.factor(Segment)))+
+  ggtitle("DSC") +
+  xlab("Temp/�C") +
+  ylab("DSC/(mW/mg)")+
+  xlim(500,1150)+
+  scale_fill_brewer(palette="Set3")
+
+
+ggplot(
+  data = Heat1) + 
+  geom_line(aes(x=Temp, y=DSC))+
+  ggtitle("DSC") +
+  xlab("Temp/�C") +
+  ylab("DSC/(mW/mg)")+
+  xlim(500,1150)
+
+ggplot(
+  data = Cool) + 
+  geom_line(aes(x=Temp, y=DSC))+
+  ggtitle("DSC") +
+  xlab("Temp/�C") +
+  ylab("DSC/(mW/mg)")+
+  xlim(500,1150)
+
+ggplot(
+  data = Heat2) + 
+  geom_line(aes(x=Temp, y=DSC))+
+  ggtitle("DSC") +
+  xlab("Temp/�C") +
+  ylab("DSC/(mW/mg)")+
+  xlim(500,1150)
 
 library("jsonlite")
 data <- read_json(arxiv-metadata.json, simplifyVector = TRUE)
